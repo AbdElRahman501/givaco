206f70baa4d5f82532d4f2741850685b69a89081
abdelrahman <bedo.ahmed416@gmail.com>
2022-07-13 18:57:01 +0200

    edit cart reducer 01

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,6 +1,20 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstant";
 
-export const cartReducer = ( state = { cartItems:[]}, action) => {
+export const cartReducer = ( state = { cartItems:[
+    {
+        id: "",
+        title : "",
+        content: "",
+        category: "" ,
+        prand: "",
+        rating: "" ,
+        numberOfRating: "" ,
+        price : "",
+        image : "",
+        inStoke : "",
+        product: "",
+    }
+]}, action) => {
     switch(action.type){
         case CART_ADD_ITEM:
             const item = action.payload;
