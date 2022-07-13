17a7a7070f3fc4cd72663721cb69fc3fca281bd1
abdelrahman <bedo.ahmed416@gmail.com>
2022-07-13 20:28:28 +0200

    store json proplem

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,7 @@ import{ productDetailsReducer, productListReducer} from './reducers/productReduc
 
 const initialState = {
     cart: {cartItems: localStorage.getItem("cartItems")
-    ? JSON.parse(localStorage.getItem("cartItems")) 
+    ? JSON.parse(localStorage.getItem("cartItems").trim()) 
     : null },
 }
 
