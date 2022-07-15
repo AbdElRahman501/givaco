15a7cfe739a0f26e43a7f134f6afd57cf723ebc2
abdelrahman <bedo.ahmed416@gmail.com>
2022-07-15 17:46:00 +0200

    star edit

--- client/src/components/products.js
+++ client/src/components/products.js
@@ -12,8 +12,11 @@ function Products(props){
             <p className="content">{content.length>25 ? content.slice(0,25)+"...":content }</p>
             <Link to={"/cart/"+_id+"?qty=1&color=1&size=1"}>
             <button> <img className="icon" src="images/icons/fi-rr-arrow-small-right.png" alt="" /> </button></Link>
+            <div className="rating">
             <img className="icon star-icon" src="images/icons/Star.png" alt="" /> 
-            <p className="rating"> {rating} | {numberOfRating}</p>
+            <p > {rating} | {numberOfRating}</p>
+            </div>
+            
             <h3 className="price">{price} EGP</h3>
             </div>
     </div> 
